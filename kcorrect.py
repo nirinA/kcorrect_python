ec0e9b2e2e4aba0e77ddf49cb127663b45d12b1f
nirinA <nirina@supernova.org>
2013-10-10 06:07:16 +0300

    dd reconstruct maggies

--- kcorrect.py
+++ kcorrect.py
@@ -18,11 +18,12 @@ except KeyError:
     raise SystemExit('Aborting! KCORRECT_DIR must be set')
 
 import _kcorrect
-import numpy as np
+import numpy
 
 def load_templates(*args):
     vfile = "/mnt/big/store/kcorrect/data/templates/vmatrix.default.dat"
     lfile = "/mnt/big/store/kcorrect/data/templates/lambda.default.dat"
+    band_shift = 0
     _kcorrect.load_templates(vfile, lfile)
 
 def load_filters(*args):
@@ -34,4 +35,21 @@ def fit_coeffs_from_file(c, *args):
     return _kcorrect.fit_coeffs_from_file(c, outfile)
 
 def fit_coeffs(c):
-    return _kcorrect.fit_coeffs(np.array(c, dtype='float32'))
+    return _kcorrect.fit_coeffs(numpy.array(c, dtype='float32'))
+
+def reconstruct_maggies(c):
+    band_shift = 0.
+    redshift = 0.
+    c = numpy.array(c, dtype='float32')
+    return _kcorrect.reconstruct_maggies(c) ##, band_shift, redshift)
+
+def reconstruct_maggies_from_file(c):
+    outfile = 'reconstructed_mag.dat'
+    with open(outfile, 'w') as out:
+        with open(c) as coeffs:
+            for l in coeffs.readlines():
+                l = list(map(float, l.strip('\n').split()))
+                l = numpy.array(l, dtype='float32')
+                r = reconstruct_maggies(l)
+                out.write('%e %e %e %e %e %e\n'%tuple(r))
+
--- src/_kcorrectmodule.c
+++ src/_kcorrectmodule.c
@@ -36,9 +36,8 @@ static float *chi2=NULL;
 float *pyvector_to_Carrayptrs(PyArrayObject *arrayin);
 
 float *pyvector_to_Carrayptrs(PyArrayObject *arrayin)  {
-/*    int n;
-    n=arrayin->dimensions[0];*/
-    return (float *) arrayin->data;  /* pointer to arrayin data as float */
+    /* pointer to arrayin data as float */
+    return (float *) arrayin->data; 
 }
 
 static PyObject *
@@ -49,17 +48,17 @@ kcorrect_load_templates(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "ss", &vfile, &lfile))
         return NULL;
 
-	/* read in templates */
-	k_read_ascii_table(&vmatrix,&ndim,&sizes,vfile);
-	nl=sizes[1];
-	nv=sizes[0];
-	FREEVEC(sizes);
-	k_read_ascii_table(&lambda,&ndim,&sizes,lfile);
-	if ((sizes[0]!=nl+1)  && PyErr_Occurred()) {
-		PyErr_SetString( kcorrectError,"vmatrix and lambda files incompatible.\n");
-		return NULL;;
-	} /* end if */
-	FREEVEC(sizes);
+    /* read in templates */
+    k_read_ascii_table(&vmatrix,&ndim,&sizes,vfile);
+    nl=sizes[1];
+    nv=sizes[0];
+    FREEVEC(sizes);
+    k_read_ascii_table(&lambda,&ndim,&sizes,lfile);
+    if ((sizes[0]!=nl+1)  && PyErr_Occurred()) {
+            PyErr_SetString( kcorrectError,"vmatrix and lambda files incompatible.\n");
+            return NULL;;
+    } /* end if */
+    FREEVEC(sizes);
 
     Py_INCREF(Py_None);
     return Py_None;
@@ -77,21 +76,21 @@ kcorrect_load_filters(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s", &ffile))
         return NULL;
 
-	/* load in the filters */
-	k_load_filters(&filter_n,&filter_lambda,&filter_pass,&maxn,&nk,ffile);
+    /* load in the filters */
+    k_load_filters(&filter_n,&filter_lambda,&filter_pass,&maxn,&nk,ffile);
 
-	/* create the rmatrix; this is a big matrix which tabulates the
-     projection of each basis element b onto each filter k, as a
-     function of redshift; you only have to project onto the filters
-     here, since every other spectrum you will project onto the
-     filters will be a linear combination of the basis elements b; you
-     interpolate the rmatrix to get a particular redshift */
-	rmatrix=(float *) malloc(nz*nv*nk*sizeof(float));
-	zvals=(float *) malloc(nz*sizeof(float));
-	for(i=0;i<nz;i++)
-		zvals[i]=zmin+(zmax-zmin)*((float)i+0.5)/(float)nz;
-	k_projection_table(rmatrix,nk,nv,vmatrix,lambda,nl,zvals,nz,filter_n,
-                           filter_lambda,filter_pass,band_shift,maxn);
+    /* create the rmatrix; this is a big matrix which tabulates the
+ projection of each basis element b onto each filter k, as a
+ function of redshift; you only have to project onto the filters
+ here, since every other spectrum you will project onto the
+ filters will be a linear combination of the basis elements b; you
+ interpolate the rmatrix to get a particular redshift */
+    rmatrix=(float *) malloc(nz*nv*nk*sizeof(float));
+    zvals=(float *) malloc(nz*sizeof(float));
+    for(i=0;i<nz;i++)
+            zvals[i]=zmin+(zmax-zmin)*((float)i+0.5)/(float)nz;
+    k_projection_table(rmatrix,nk,nv,vmatrix,lambda,nl,zvals,nz,filter_n,
+                       filter_lambda,filter_pass,band_shift,maxn);
     
     Py_INCREF(Py_None);
     return Py_None;
@@ -106,7 +105,7 @@ kcorrect_fit_coeffs_from_file(PyObject *self, PyObject *args)
 {
     IDL_LONG i,j,k,niter,nchunk,ncurrchunk;
     char * cfile, *ofile;
-    PyArrayObject *cin, *cout;
+    PyArrayObject *cin;
     PyArray_Descr * dsc;
     FILE *fp, *ofp;
     int nd;
@@ -117,20 +116,20 @@ kcorrect_fit_coeffs_from_file(PyObject *self, PyObject *args)
     ofp=fopen(ofile,"w");
     dsc = PyArray_DescrFromType(NPY_FLOAT32);
     cin = (PyArrayObject *)PyArray_FromFile(fp, dsc, -1, " ");
+    fclose(fp);
     nd=cin->dimensions[0]/11;
     nchunk=nd;
     float *mag;
     mag=pyvector_to_Carrayptrs(cin);
-	redshift=(float *) malloc((nchunk+1)*sizeof(float));
-	maggies=(float *) malloc((nchunk+1)*nk*sizeof(float));
-	maggies_ivar=(float *) malloc((nchunk+1)*nk*sizeof(float));
-	coeffs=(float *) malloc((nchunk+1)*nv*sizeof(float));
-	chi2=(float *) malloc((nchunk+1)*sizeof(float));
+    redshift=(float *) malloc((nchunk+1)*sizeof(float));
+    maggies=(float *) malloc((nchunk+1)*nk*sizeof(float));
+    maggies_ivar=(float *) malloc((nchunk+1)*nk*sizeof(float));
+    coeffs=(float *) malloc((nchunk+1)*nv*sizeof(float));
+    chi2=(float *) malloc((nchunk+1)*sizeof(float));
 
     for (i=0;i<nd && i<nchunk;i++) {
         redshift[i] = mag[i*11];
         for(k=0;k<nk;k++) {
-            //printf("i: %i m %i  mi %i ...\n", i, ((i*11)+1)+k, ((i*11)+6)+k);
             maggies[i*nk+k] = mag[((i*11)+1)+k];
             maggies_ivar[i*nk+k] = mag[((i*11)+6)+k];
         }
@@ -138,15 +137,16 @@ kcorrect_fit_coeffs_from_file(PyObject *self, PyObject *args)
 
     ncurrchunk=nd;
     /* no direct constraints on the coeffs are included in this fit */
-		k_fit_nonneg(coeffs,rmatrix,nk,nv,zvals,nz,maggies,
-                             maggies_ivar,redshift,ncurrchunk,tolerance,
-                             maxiter,&niter,chi2,0,0);
-		for(i=0;i<ncurrchunk;i++) {
-			fprintf(ofp,"%e ",redshift[i]);
-			for(j=0;j<nv;j++)
-				fprintf(ofp,"%e ",coeffs[i*nv+j]);
-			fprintf(ofp,"\n");
-		} /* end for i */
+    k_fit_nonneg(coeffs,rmatrix,nk,nv,zvals,nz,maggies,
+                 maggies_ivar,redshift,ncurrchunk,tolerance,
+                 maxiter,&niter,chi2,0,0);
+    for(i=0;i<ncurrchunk;i++) {
+            fprintf(ofp,"%e ",redshift[i]);
+            for(j=0;j<nv;j++)
+                    fprintf(ofp,"%e ",coeffs[i*nv+j]);
+            fprintf(ofp,"\n");
+    } /* end for i */
+    fclose(ofp);
 
     FREEVEC(redshift);
     FREEVEC(maggies);
@@ -159,16 +159,16 @@ kcorrect_fit_coeffs_from_file(PyObject *self, PyObject *args)
 }
 
 PyDoc_STRVAR(kcorrect_fit_coeffs_from_file_doc,
-"prints the redshifts and coeffs "
+"writes the computed coeffs onto a file\n\
+with redshift and maggies read from a given file"
 );
 
 static PyObject *
 kcorrect_fit_coeffs(PyObject *self, PyObject *args)
 {
-    IDL_LONG i,j,k,niter;
+    IDL_LONG j,niter;
     PyArrayObject *pyin, *pyout;
     float *cin, *cout;
-    int nd;
     npy_intp dims[] = {6};
     PyArray_Descr * dsc;
     dsc = PyArray_DescrFromType(NPY_FLOAT32);
@@ -176,27 +176,23 @@ kcorrect_fit_coeffs(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &pyin))
         return NULL;
     if (NULL == pyin)  return NULL;
-    nd=pyin->dimensions[0];
-    //dims = pyin->dimensions;
-//printf("nd = %i, %i, %f\n", pyin->nd, (int)pyin->dimensions[0], pyin->data[0]);
-        pyout=(PyArrayObject *) PyArray_NewFromDescr(&PyArray_Type,             
-                                                     dsc,
-                                                     1,
-                                                     dims,
-                                                     NULL,
-                                                     NULL,
-                                                     0,
-                                                     NULL);
-
+    pyout=(PyArrayObject *) PyArray_NewFromDescr(&PyArray_Type,
+                                                 dsc,
+                                                 1,
+                                                 dims,
+                                                 NULL,
+                                                 NULL,
+                                                 0,
+                                                 NULL);
     if (NULL == pyout)  return NULL;
-	cin=pyvector_to_Carrayptrs(pyin);
-	cout=pyvector_to_Carrayptrs(pyout);
+    cin=pyvector_to_Carrayptrs(pyin);
+    cout=pyvector_to_Carrayptrs(pyout);
 
-	redshift=(float *) malloc(sizeof(float));
-	maggies=(float *) malloc(nk*sizeof(float));
-	maggies_ivar=(float *) malloc(nk*sizeof(float));
-	coeffs=(float *) malloc(nv*sizeof(float));
-	chi2=(float *) malloc(sizeof(float));
+    redshift=(float *) malloc(sizeof(float));
+    maggies=(float *) malloc(nk*sizeof(float));
+    maggies_ivar=(float *) malloc(nk*sizeof(float));
+    coeffs=(float *) malloc(nv*sizeof(float));
+    chi2=(float *) malloc(sizeof(float));
 
     cout[0] = redshift[0] = cin[0];
     maggies[0] = cin[1];
@@ -211,15 +207,13 @@ kcorrect_fit_coeffs(PyObject *self, PyObject *args)
     maggies_ivar[4] = cin[10];
 
     /* no direct constraints on the coeffs are included in this fit */
-		k_fit_nonneg(coeffs,rmatrix,nk,nv,zvals,nz,maggies,
-                             maggies_ivar,redshift,1,tolerance,
-                             maxiter,&niter,chi2,0,0);
-
+    k_fit_nonneg(coeffs,rmatrix,nk,nv,zvals,nz,
+                 maggies,maggies_ivar,redshift,
+                 1,tolerance,maxiter,&niter,chi2,0,0);
 /*
     fprintf(stdout,"%e ",redshift[0]);
-    for(j=0;j<nv;j++)    fprintf(stdout,"%e ",coeffs[j+nv]);
+    for(j=0;j<nv;j++)    fprintf(stdout,"%e ",coeffs[j]);
     fprintf(stdout,"\n");
-
     Py_INCREF(Py_None);
     return Py_None;
 */
@@ -236,14 +230,69 @@ kcorrect_fit_coeffs(PyObject *self, PyObject *args)
 }
 
 PyDoc_STRVAR(kcorrect_fit_coeffs_doc,
-"prints the redshifts and coeffs "
+"computes the coeffs from given maggies and redshift"
+);
+
+static PyObject *
+kcorrect_reconstruct_maggies(PyObject *self, PyObject *args)
+{
+    IDL_LONG j;
+    PyArrayObject *pyin, *pyout;
+    float *cin, *cout;
+    npy_intp dims[] = {6};
+    PyArray_Descr * dsc;
+    dsc = PyArray_DescrFromType(NPY_FLOAT32);
+    //float b_shift, r;
+    if (!PyArg_ParseTuple(args, "O!", /*"O!dd",*/
+                          &PyArray_Type, &pyin)) //,
+                          /* &b_shift, &r)) not used for now */
+        return NULL;
+    if (NULL == pyin)  return NULL;
+    pyout=(PyArrayObject *) PyArray_NewFromDescr(&PyArray_Type,             
+                                                 dsc,
+                                                 1,
+                                                 dims,
+                                                 NULL,
+                                                 NULL,
+                                                 0,
+                                                 NULL);
+    if (NULL == pyout)  return NULL;
+    cin=pyvector_to_Carrayptrs(pyin);
+    cout=pyvector_to_Carrayptrs(pyout);
+
+    redshift=(float *) malloc(sizeof(float));
+    maggies=(float *) malloc(nk*sizeof(float));
+    coeffs=(float *) malloc(nv*sizeof(float));
+    cout[0] = redshift[0] = cin[0];
+
+    //if(all_redshift!=-1.) redshift[0]=all_redshift;
+
+    coeffs[0] = cin[1];
+    coeffs[1] = cin[2];
+    coeffs[2] = cin[3];
+    coeffs[3] = cin[4];
+    coeffs[4] = cin[5];
+    k_reconstruct_maggies(zvals,nz,rmatrix,nk,nv,coeffs,redshift,maggies,1);
+    for(j=0;j<nv;j++)   cout[1+j] = maggies[j];
+
+    FREEVEC(redshift);
+    FREEVEC(maggies);
+    FREEVEC(coeffs);
+    
+    Py_INCREF(pyout); 
+    return PyArray_Return(pyout);                     
+}
+
+PyDoc_STRVAR(kcorrect_reconstruct_maggies_doc,
+"reconstructs maggies from given coeffs and redshift"
 );
 
 static PyMethodDef kcorrect_methods[] = {
-    {"fit_coeffs", kcorrect_fit_coeffs, METH_VARARGS, kcorrect_fit_coeffs_doc},
-    {"fit_coeffs_from_file", kcorrect_fit_coeffs_from_file, METH_VARARGS, kcorrect_fit_coeffs_from_file_doc},
     {"load_templates", kcorrect_load_templates, METH_VARARGS, kcorrect_load_templates_doc},
     {"load_filters", kcorrect_load_filters, METH_VARARGS, kcorrect_load_filters_doc},
+    {"fit_coeffs_from_file", kcorrect_fit_coeffs_from_file, METH_VARARGS, kcorrect_fit_coeffs_from_file_doc},
+    {"fit_coeffs", kcorrect_fit_coeffs, METH_VARARGS, kcorrect_fit_coeffs_doc},
+    {"reconstruct_maggies", kcorrect_reconstruct_maggies, METH_VARARGS, kcorrect_reconstruct_maggies_doc},
     {NULL,		NULL}		/* sentinel */
 };
 
